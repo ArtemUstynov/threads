95e9cc8d0fe909f1823aa1509a6ccb357fea936c
Artem Ustynov <ustynov@merck.com>
2020-01-27 13:00:34 +0100

    threads

---
+++ sample_tester.cpp
@@ -0,0 +1,153 @@
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include "sample_tester.h"
+#include <chrono>
+
+using namespace std;
+
+//=================================================================================================
+CProducerSync::CProducerSync(function<void(AProducer, APriceList)> receiver)
+        : m_Receiver(receiver) {
+}
+
+//-------------------------------------------------------------------------------------------------
+void CProducerSync::SendPriceList(unsigned materialID) {
+     this_thread::sleep_for(chrono::milliseconds(1000));
+    if (materialID == 1) {
+        APriceList l = make_shared<CPriceList>(1);
+        for (const auto & x : c_Prod)
+            l->Add(x);
+        m_Receiver(shared_from_this(), l);
+    }
+}
+
+//-------------------------------------------------------------------------------------------------
+vector<CProd> CProducerSync::c_Prod =
+        {
+                //CProd(1, 1, 5), //!!!!
+                CProd(1, 1, 10),
+                CProd(2, 7, 120),
+                CProd(11, 8, 950),
+
+                /*CProd(2, 7, 125),
+                CProd(3, 5, 150),
+                CProd(7, 3, 240),
+                CProd(4, 4, 155)*/
+        };
+
+
+//=================================================================================================
+CProducerAsync::CProducerAsync(function<void(AProducer, APriceList)> receiver)
+        : m_Receiver(receiver),
+          m_Req(0),
+          m_Stop(false) {
+}
+
+//-------------------------------------------------------------------------------------------------
+void CProducerAsync::Start(void) {
+    // cout << "asynk producer start" << endl;
+    // printf("asynk call\n");
+    m_Thr = move(thread(&CProducerAsync::prodThr, this));
+    //printf("asynk deliver\n");
+}
+
+//-------------------------------------------------------------------------------------------------
+void CProducerAsync::Stop(void) {
+    // cout << "asynk producer stop" << endl;
+    unique_lock<mutex> locker(m_Mtx);
+    m_Stop = true;
+    m_Cond.notify_one();
+    locker.unlock();
+    m_Thr.join();
+}
+
+//-------------------------------------------------------------------------------------------------
+void CProducerAsync::SendPriceList(unsigned materialID) {
+    // cout << "asynk producer send price list" << endl;
+    //printf("asynk call\n");
+    this_thread::sleep_for(chrono::milliseconds(1000));
+    if (materialID == 1) {
+        lock_guard<mutex> locker(m_Mtx);
+        m_Req++;
+        m_Cond.notify_one();
+    }
+}
+
+//-------------------------------------------------------------------------------------------------
+void CProducerAsync::prodThr(void) {
+    // cout << "asynk producer prodthr" << endl;
+
+    int delay  = rand() % 10 + 1;
+    this_thread::sleep_for(chrono::milliseconds(delay));
+
+    while (true) {
+        unique_lock<mutex> locker(m_Mtx);
+        m_Cond.wait(locker, [this](void) { return m_Stop || m_Req > 0; });
+
+        if (m_Stop)
+            break;
+        if (m_Req > 0) {
+            m_Req--;
+            locker.unlock();
+            APriceList l = make_shared<CPriceList>(1);
+            for (const auto & x : c_Prod)
+                l->Add(x);
+            m_Receiver(shared_from_this(), l);
+        }
+    }
+}
+
+//-------------------------------------------------------------------------------------------------
+vector<CProd>      CProducerAsync::c_Prod = {
+
+       /* CProd(1, 1, 10),
+        CProd(2, 7, 120),
+        CProd(11, 8, 950),*/
+
+        CProd(2, 7, 125),
+        CProd(3, 5, 150),
+        CProd(7, 3, 240),
+        CProd(4, 4, 155)
+};
+
+//=================================================================================================
+CCustomerTest::CCustomerTest(unsigned count) : m_Count(count) {
+    // cout << "created customer" << endl;
+}
+
+//-------------------------------------------------------------------------------------------------
+AOrderList CCustomerTest::WaitForDemand(void) {
+    int delay  = rand() % 10 + 1;
+    this_thread::sleep_for(chrono::milliseconds(delay));
+    if (!m_Count)
+        return AOrderList();
+    m_Count--;
+    AOrderList req = make_shared<COrderList>(1);
+    for (const auto & x : c_Orders)
+        req->Add(x.first);
+    return req;
+}
+
+//-------------------------------------------------------------------------------------------------
+void CCustomerTest::Completed(AOrderList x) {
+    // cout << "customer test1 completed" << endl;
+    bool mismatch = false;
+
+    for (size_t i = 0; i < c_Orders.size(); i++)
+        if (fabs(c_Orders[i].second - x->m_List[i].m_Cost) > 1e-5 * c_Orders[i].second) {
+            mismatch = true;
+            break;
+        }
+    printf("CCustomerTest::Completed, status = %s\n", mismatch ? "fail" : "OK");
+}
+
+//-------------------------------------------------------------------------------------------------
+vector<pair<COrder, double> > CCustomerTest::c_Orders =
+        {
+                make_pair(COrder(2, 2, 0.0), 40.0),
+                make_pair(COrder(7, 12, 1.0), 755.0),
+                make_pair(COrder(8, 4, 10.0), 350.0),
+                make_pair(COrder(25, 11, 0.1), 2399.8)
+        };
+
